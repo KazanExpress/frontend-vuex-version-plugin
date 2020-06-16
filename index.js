c9cae2fbb2f88ac3287fcfb253e08ae7c4a74305
Karen <dev.kaskar2008@gmail.com>
2020-06-16 12:12:16 +0300

    Add versionPlugin

---
+++ index.js
@@ -0,0 +1,15 @@
+export const versionPlugin = ({ version, name, action } = {}) => {
+  return !process.server ? (store) => {
+    const currentVersion = version || 1;
+    const itemKey = name || 'application-store-version';
+    const localVersion = parseInt(localStorage.getItem(itemKey)) || 0;
+    if (!localVersion || localVersion < currentVersion) {
+      localStorage.clear();
+      localStorage.setItem(itemKey, currentVersion);
+
+      if (action && store._actions[action]) {
+        store.dispatch(action);
+      }
+    }
+  } : () => {};
+};
